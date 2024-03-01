f585689ce8dbddc296c36dd7ec338f67a60565d0
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-03-01 12:43:10 +0100

    Add files via upload

---
+++ Northwind.Webb/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Northwind.Shared;
+
+
+namespace Northwind.Webb.Pages
+{
+    public class SuppliersModel : PageModel
+    {
+        public IEnumerable<Supplier?> Suppliers { get; set; }
+
+        private NorthwindContext db;
+
+        [BindProperty]
+
+        public Supplier? Supplier { get; set; }
+
+        public SuppliersModel(NorthwindContext injectedContext)
+        {
+            db = injectedContext;
+        }
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind B2B - Suppliers";
+            Suppliers = db.Suppliers.OrderBy(c => c.Country).ThenBy(c => c.CompanyName);
+        }
+
+        public IActionResult OnPost()
+        {
+            if ((Supplier is not null)&& ModelState.IsValid)
+            {
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
---
+++ Northwind.Webb/Program.cs
@@ -0,0 +1,22 @@
+using Northwind.Common.DataContext.sqlServer;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddNorthwindContext();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapRazorPages();
+app.MapGet("/hello", () => "Hello World!");
+
+app.Run();
+
+Console.WriteLine("This executes after the web server has stopped!");
